934b43e04ec037e239185d2d9f85cf7315d6cf09
ImkePapsPXL <Imke.Paps@student.pxl.be>
2024-10-04 10:36:56 +0200

    KLEUR-03

--- taak 1/taak 1/MainWindow.xaml.cs
+++ taak 1/taak 1/MainWindow.xaml.cs
@@ -20,5 +20,49 @@ namespace taak_1
         {
             InitializeComponent();
         }
+
+        private void red_MouseEnter(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = Brushes.Red;
+            
+        }
+
+        private void green_MouseEnter(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = Brushes.Green;
+        
+        }
+
+        private void yellow_MouseEnter(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = Brushes.Yellow;
+            
+        }
+
+        private void blue_MouseEnter(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = Brushes.Blue;
+           
+        }
+
+        private void red_MouseLeave(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = Brushes.LightGray;
+        }
+
+        private void green_MouseLeave(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = Brushes.LightGray;
+        }
+
+        private void yellow_MouseLeave(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = Brushes.LightGray;
+        }
+
+        private void blue_MouseLeave(object sender, MouseEventArgs e)
+        {
+            mainWindow.Background = Brushes.LightGray;
+        }
     }
 }
